c599c4aed862c4f9c324a1563d5027bc048df459
Jauharuddin <jauharnotes@gmail.com>
2022-01-16 20:04:25 +0700

    Update App.js

--- passing-state-as-props/App.js
+++ passing-state-as-props/App.js
@@ -11,16 +11,6 @@ export default function App() {
         setCount(prevCount => prevCount - 1)
     }
     
-    /**
-     * Challenge:
-     * - Create a new component named Count
-     *    - It should receive a prop called `number`, whose value
-     *      is the current value of our count
-     *    - Have the component render the whole div.counter--count
-     *      and display the incoming prop `number`
-     * - Replace the div.counter--count below with an instance of
-     *   the new Count component
-     */
     return (
         <div className="counter">
             <button className="counter--minus" onClick={subtract}>–</button>
